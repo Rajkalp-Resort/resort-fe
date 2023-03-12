9742401e01162e15fae230fe464758ee2e26cc58
Nitish <nitish.bpns@gmail.com>
2023-03-13 03:24:04 +0530

    cmit3

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ScrollToTop } from 'react-router-scroll-to-top';
 import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Router>
+      <ScrollToTop />
+      <Routes>
 
-        <p>
-          Rajkalp resort
-        </p>
+        <Route path='/' exact element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
 
-
-      </header>
-    </div >
+      </Routes>
+    </Router >
   );
 }
+
+
+
+
 
 export default App;
